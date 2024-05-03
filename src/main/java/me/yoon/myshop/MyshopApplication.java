9744131cb19af44715f78b2930f52743c7dab292
uniqquej <cjy4477@gmail.com>
2024-05-03 23:56:31 +0900

    init

---
+++ src/main/java/me/yoon/myshop/MyshopApplication.java
@@ -0,0 +1,11 @@
+package me.yoon.myshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyshopApplication.class, args);
+    }
+}
