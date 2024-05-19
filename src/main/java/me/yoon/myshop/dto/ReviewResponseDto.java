adb79d0985762103c5d777f3cb79c9fe14b1583c
uniqquej <cjy4477@gmail.com>
2024-05-20 05:24:05 +0900

    STYLE: 리뷰 조회 페이지 style 수정

--- src/main/java/me/yoon/myshop/dto/ReviewResponseDto.java
+++ src/main/java/me/yoon/myshop/dto/ReviewResponseDto.java
@@ -5,25 +5,31 @@ import lombok.Setter;
 import me.yoon.myshop.entity.Review;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 @Getter @Setter
 public class ReviewResponseDto {
     private String writer;
     private int star;
     private String content;
-    private LocalDateTime registerTime;
+    private String registerTime;
 
     public ReviewResponseDto(Review review){
         String[] emailFront = review.getUser().getEmail().split("@")[0].split("");
+        System.out.println("*********************************"+ Arrays.toString(emailFront));
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i< emailFront.length-3;i++){
-            sb.append(emailFront[i]);
+        if(emailFront.length<=2) sb.append("*");
+        else {
+            for(int i=0; i< emailFront.length-3;i++){
+                sb.append(emailFront[i]);
+            }
+            sb.append("**");
         }
-        sb.append("**");
 
         this.writer = sb.toString();
         this.content = review.getContent();
         this.star = review.getStar();
-        this.registerTime = getRegisterTime();
+        this.registerTime = review.getRegisterTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));;
     }
 }
